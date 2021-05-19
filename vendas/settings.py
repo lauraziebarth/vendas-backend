9b29e0143ebb35b903f733466ece883940da5e9d
Laura Ziebarth <lauragzie@gmail.com>
2021-05-19 00:19:37 -0300

    adiciona rest_framework nos apps

--- vendas/settings.py
+++ vendas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'clientes.apps.ClientesConfig',
     'produtos.apps.ProdutosConfig',
     'colaboradores.apps.ColaboradoresConfig',
