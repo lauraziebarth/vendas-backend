07972490778e488d5b51b11313d521cd3a2581e0
Laura Ziebarth <lauragzie@gmail.com>
2021-05-19 00:20:28 -0300

    refaz views de clientes

---
+++ produtos/views.py
@@ -0,0 +1,12 @@
+
+from rest_framework import viewsets
+
+from produtos.gateway import busca_todos_os_produtos
+from produtos.serializer import ProdutoSerializer
+
+
+class ProdutosViewSet(viewsets.ModelViewSet):
+    queryset = busca_todos_os_produtos()
+    serializer_class = ProdutoSerializer
+
+
