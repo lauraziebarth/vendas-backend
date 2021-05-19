aa9122400f7f0c7dfd4d33f904cff57a232e7bc7
Laura Ziebarth <lauragzie@gmail.com>
2021-05-19 00:19:59 -0300

    cria urls de clientes e produtos

--- vendas/urls.py
+++ vendas/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from clientes.views import ClientesViewSet
+from produtos.views import ProdutosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('clientes', ClientesViewSet, basename='Clientes')
+router.register('produtos', ProdutosViewSet, basename='Produtos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
