fff432c1b3a1af755704f752a391b772bbdea687
Laura Ziebarth <lauragzie@gmail.com>
2021-05-19 00:18:13 -0300

    adiciona busca geral de produtos

--- produtos/gateway.py
+++ produtos/gateway.py
@@ -4,3 +4,6 @@ from produtos.models import Produto
 def busca_um_produto(produto_id):
     return Produto.objects.get(id=produto_id)
 
+
+def busca_todos_os_produtos():
+    return Produto.objects.all()
