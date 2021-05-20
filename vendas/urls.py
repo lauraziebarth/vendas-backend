c1100d8f86e56592155c73ac36ee6af504bda1b2
lauraziebarth <lauragzie@gmail.com>
2021-05-20 13:41:12 -0300

    atualiza urls

--- vendas/urls.py
+++ vendas/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
 from clientes.views import ClientesViewSet
 from produtos.views import ProdutosViewSet
-from rest_framework import routers
+from pedidos.views import PedidosViewSet
 
 router = routers.DefaultRouter()
 router.register('clientes', ClientesViewSet, basename='Clientes')
 router.register('produtos', ProdutosViewSet, basename='Produtos')
+router.register('pedidos', PedidosViewSet, basename='Pedidos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
