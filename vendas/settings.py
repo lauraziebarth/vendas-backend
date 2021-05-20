69d247ea97a8be8aec178507cec7457b776f887a
lauraziebarth <lauragzie@gmail.com>
2021-05-20 13:41:53 -0300

    adiciona pedidos em apps

--- vendas/settings.py
+++ vendas/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'pedidos.apps.PedidosConfig',
     'clientes.apps.ClientesConfig',
     'produtos.apps.ProdutosConfig',
     'colaboradores.apps.ColaboradoresConfig',
