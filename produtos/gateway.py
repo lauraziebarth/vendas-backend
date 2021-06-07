39ff23e1f5a9ae4d8032a180ced318b5c5eca771
Laura Ziebarth <lauragzie@gmail.com>
2021-06-07 20:19:13 -0300

    altera a busca de produtos  geral para apenas não excluidos

--- produtos/gateway.py
+++ produtos/gateway.py
@@ -7,3 +7,7 @@ def busca_um_produto(produto_id):
 
 def busca_todos_os_produtos():
     return Produto.objects.all()
+
+
+def busca_produtos_nao_excluidos():
+    return Produto.objects.filter(excluido=False)
--- produtos/views.py
+++ produtos/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
 
-from produtos.gateway import busca_todos_os_produtos
+from produtos.gateway import busca_produtos_nao_excluidos
 from produtos.serializer import ProdutoSerializer
 
 
 class ProdutosViewSet(viewsets.ModelViewSet):
-    queryset = busca_todos_os_produtos()
+    queryset = busca_produtos_nao_excluidos()
     serializer_class = ProdutoSerializer
 
 
