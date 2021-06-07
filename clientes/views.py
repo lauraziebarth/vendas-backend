09c7013ebfec876c4c02f84bc7969a76549082a6
Laura Ziebarth <lauragzie@gmail.com>
2021-06-07 20:18:39 -0300

    altera a busca de clientes geral para apenas não excluidos

--- clientes/views.py
+++ clientes/views.py
@@ -1,11 +1,11 @@
-from clientes.gateway import busca_todos_os_clientes
-
 from rest_framework import viewsets
 from clientes.serializer import ClienteSerializer
 
+from clientes.gateway import busca_clientes_nao_excluidos
+
 
 class ClientesViewSet(viewsets.ModelViewSet):
-    queryset = busca_todos_os_clientes()
+    queryset = busca_clientes_nao_excluidos()
     serializer_class = ClienteSerializer
 
 
