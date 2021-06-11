f22c0802f2a6a5e22dba9f03d023fb176418b8f2
lauraziebarth <lauragzie@gmail.com>
2021-06-11 15:41:29 -0300

    ajusta field cliente

--- pedidos/serializer.py
+++ pedidos/serializer.py
@@ -4,10 +4,9 @@ from pedidos.models import Pedido
 from item_pedido.serializer import ItemPedidoSerializer
 
 
-
 class PedidoSerializer(serializers.ModelSerializer):
     itens = ItemPedidoSerializer(read_only=True, many=True)
 
     class Meta:
         model = Pedido
-        fields = ('id', 'cliente_id', 'cliente_nome', 'condicao_pagamento', 'total', 'itens')
+        fields = ('id', 'cliente', 'cliente_nome', 'condicao_pagamento', 'total', 'itens')
