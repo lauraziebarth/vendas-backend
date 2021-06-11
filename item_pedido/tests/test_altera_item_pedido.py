026a86f1995af63f02738dd6166ebb3e145878a3
lauraziebarth <lauragzie@gmail.com>
2021-06-11 17:34:18 -0300

    ajusta test_altera_itens_pedido

--- item_pedido/tests/test_altera_item_pedido.py
+++ item_pedido/tests/test_altera_item_pedido.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from item_pedido.crud import atualiza_itens_pedido, cria_item_pedido
+from item_pedido.crud import atualiza_itens_pedido, cria_item_pedido, busca_todos_os_itens_de_um_pedido
 from pedidos.serializer import PedidoSerializer
 
 
@@ -12,11 +12,11 @@ class AlteraItensPedidoTests(TestCase):
                                          'total': 750009.87, 'rentabilidade': 'great'}]}
 
         self.pedido_novo = {'id': 1, 'cliente_id': 1, 'cliente_nome': 'Han Solo', 'condicao_pagamento': '30/60', 'total': 750009.87,
-                            'itens': [{'produto_id': 4, 'produto_nome': 'TIE Fighter', 'produto_multiplo': 2,
+                            'itens': [{'produto_id': 5, 'produto_nome': 'Lightsaber', 'produto_multiplo': 2,
                                        'produto_preco_tabela': 75000, 'quantidade': 8, 'preco_liquido': 75000.99,
                                        'total': 60007.92, 'rentabilidade': 'great', 'id':1}]}
 
-    def test_altera_item_pedido(self):
+    def test_altera_itens_pedido(self):
         serializer_pedido_antigo = PedidoSerializer(self.pedido_antigo)
         pedido_id_antigo = serializer_pedido_antigo.data.get('id')
         itens_antigos = serializer_pedido_antigo.data.get('itens')
@@ -25,9 +25,13 @@ class AlteraItensPedidoTests(TestCase):
         serializer_pedido_novo = PedidoSerializer(self.pedido_novo)
         pedido_id = serializer_pedido_novo.data.get('id')
         itens_novos = serializer_pedido_novo.data.get('itens')
-        result = atualiza_itens_pedido(pedido_id, itens_novos)
+        atualiza_itens_pedido(pedido_id, itens_novos)
 
-        self.assertEqual(result['produto_nome'], 'TIE Fighter')
-        self.assertEqual(result['produto_multiplo'], 2)
-        self.assertEqual(result['quantidade'], 8)
-        self.assertEqual(result['produto_id'], 4)
+        result = busca_todos_os_itens_de_um_pedido(pedido_id).count()
+        result_list = busca_todos_os_itens_de_um_pedido(pedido_id)
+
+        self.assertEqual(result, 1)
+        self.assertEqual(result_list[0].id, 1)
+        self.assertEqual(result_list[0].produto_id, 5)
+        self.assertEqual(result_list[0].produto_nome, 'Lightsaber')
+        self.assertEqual(result_list[0].quantidade, 8)
